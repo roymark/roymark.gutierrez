7765b3d7cf564a93ee023001d4927cfd0e73b963
Roy Mark J. Gutierrez <roymark.gutierrez@gmail.com>
2017-08-28 03:36:52 +0800

    Update OCS-Panel.sh

--- OCS-Panel.sh
+++ OCS-Panel.sh
@@ -15,8 +15,8 @@ apt-get -y install nginx php5 php5-fpm php5-cli php5-mysql php5-mcrypt
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
-curl https://raw.githubusercontent.com/anggasa/worm/master/nginx.conf > /etc/nginx/nginx.conf
-curl https://raw.githubusercontent.com/jhelson15/re-construction/master/conf/vps.conf > /etc/nginx/conf.d/vps.conf
+curl https://raw.githubusercontent.com/roymark/gutierrez/master/nginx.conf > /etc/nginx/nginx.conf
+curl https://raw.githubusercontent.com/roymark/roymark.gutierrez/master/vps.conf > /etc/nginx/conf.d/vps.conf
 sed -i 's/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 useradd -m vps
